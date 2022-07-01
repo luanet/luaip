8383b33ee628802a166e85d77a2cce06bda9e62d
Lua Network <106291312+luanet@users.noreply.github.com>
2022-07-01 10:18:41 +0700

    Get proxy forwarded ip

--- icanhaz.py
+++ icanhaz.py
@@ -27,7 +27,7 @@ app = Flask(__name__, static_folder='static')
 
 def isOpen(ip, port, protocol):
     s = socket.socket(socket.AF_INET, protocol)
-    s.settimeout(1)
+    s.settimeout(0.5)
     try:
         s.connect((ip, int(port)))
         s.shutdown(socket.SHUT_RDWR)
@@ -40,7 +40,7 @@ def isOpen(ip, port, protocol):
 @app.route("/")
 def icanhazafunction():
     mimetype = "application/json; charset=utf-8"
-    result = request.remote_addr
+    result = request.environ['HTTP_X_FORWARDED_FOR']
     ipfs_open = "true" if isOpen(result, 4001, socket.SOCK_STREAM) & isOpen(result, 4001, socket.SOCK_DGRAM) else "false"
     gateway_open = "true" if isOpen(result, 443, socket.SOCK_STREAM) else "false"
     return Response("{\"ip\": \"%s\", \"swarm\": %s, \"gateway\": %s}" % (result, ipfs_open, gateway_open), mimetype=mimetype, headers={"X-Your-Ip": request.remote_addr})
